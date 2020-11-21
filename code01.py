b2ee649e29200d15a852ca9e4247a3306e8fabdc
B179372-2020 <email_not_provided>
2020-11-21 16:16:06 +0000

    code:try to get the 250 most similar seq

--- code01.py
+++ code01.py
@@ -4,6 +4,7 @@
 from Bio import Entrez
 import subprocess
 import os
+import re
 
 
 def obtain_search_term():
@@ -35,7 +36,7 @@ def obtain_search_term():
 # es_com = "esearch -db protein -query \""+taxonomic_group_name+" [organism] AND "+protein_family_name+" [protein]\"|efetch -format fasta > protein_seq.fa"
 
 es_com = "esearch -db protein -query 'aves[organism] AND glucose-6-phosphatase[protein]' |efetch -format fasta > protein_seq.fa"
-subprocess.call(es_com,shell=True)
+#######subprocess.call(es_com,shell=True)
 
 #def _esearch(search_term,email,database):
     # Retrieve the data set specified by the user in the database
@@ -54,20 +55,65 @@ subprocess.call(es_com,shell=True)
 #database = "protein"
 #search_results = _esearch(search_term, email, database)
 
-def _efetch(search_results,database):
-    acc_list = search_results["IdList"]
-    file_name = "protein_seq.fasta"
-    with open (file_name,'w') as seq:
-        for _id_ in acc_list:
-            #print(_id_)
-            handle = Entrez.efetch(db=database,id=_id_,rettype="fasta",retmode="text")
-            #print(handle.read())
-            seq.write(handle.read())  
+#def _efetch(search_results,database):
+#    acc_list = search_results["IdList"]
+#    file_name = "protein_seq.fasta"
+#    with open (file_name,'w') as seq:
+#        for _id_ in acc_list:
+#            #print(_id_)
+#            handle = Entrez.efetch(db=database,id=_id_,rettype="fasta",retmode="text")
+#            #print(handle.read())
+#            seq.write(handle.read())  
 #_efetch(search_results,database)
 
 
 ### alignment
-os.system("clustalo -i protein_seq.fa --maxnumseq 250 -o ali.fa")
-#subprocess.call()
+#os.system("clustalo -i protein_seq.fa --maxnumseq 250 -o ali.fa")
+
+def find_similar_250_seq():
+    '''
+    fine the 250 most similar protein sequences
+
+    '''
+    count_ = 0
+    count_list = [] 
+    nameline = []
+    name_list = []
+
+    with open("ali.fa","r") as alignment:
+        for line in alignment.readlines():    ### Read each line of the file
+            line = line.strip()    ### Remove spaces and tab at the beginning and end of each line
+            if re.match(r'^\>(.*)',line):
+                line = line.strip(">")     ### Remove '>' at the begining 
+                nameline = line.split()
+                name_list.append(nameline[0])    ### Extract the sequence name
+                count_list.append(count_)        ### Obtain the number of '-'
+                count_ = 0     ### The counter returns to zero and is ready to count the next '-' number
+            else:
+                count_ = count_ + line.count("-")
+    count_list.append(count_)   ### The '-' number of the last sequence was not added to the list in the loop, so it is added here
+    del count_list[0]     ### The first element in count_list[] is invalid, so delete it
+    #print(count_list,len(count_list),name_list,len(name_list))
+    
+    ### dictionary {seq_name : number of minus signs}
+    count_dict = {}
+    for i in range(len(name_list)):
+        key = name_list[i]
+        value = count_list[i]
+        count_dict[key] = value
+
+    ### Dictionary sorted by value from small to large
+    count_dict_sorted = sorted(count_dict.items(), key=lambda  kv:(kv[1],kv[0]))
+    print(count_dict_sorted)
+    
+    ### Gets index numbers of most similar seq
+    index_list = [i for i,x in enumerate(count_list) if x==min(count_list)]
+    
+    return count_dict
+#find_similar_250_seq()
+
+
+
+
 
 
