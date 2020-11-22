8cf011109aa260b6a45e052cde5c0c7b8848490a
B179372-2020 <email_not_provided>
2020-11-22 21:06:51 +0000

    code:Added interaction(total number of sequences & selection of 250 sequences)

--- code01.py
+++ code01.py
@@ -3,15 +3,14 @@
 
 from Bio import Entrez
 import subprocess
-import os
 import re
-
+import sys
 
 def obtain_search_term():
- 
-    # user specify the protein family and the taxonomic group
-    # store the name entered by user into "final_search_term"
-        
+    '''
+    User specify the protein family and the taxonomic group
+
+    '''    
     protein_family_name = input("Please enter the name of the protein family  eg. glucose-6-phosphatase:\n")
     taxonomic_group_name = input("Please enter the name of taxonomic group  eg. birds(Aves):\n")
     final_search_term = protein_family_name + " in " + taxonomic_group_name
@@ -22,73 +21,90 @@ def obtain_search_term():
             protein_family_name = input("Please enter the name of the protein family  eg. glucose-6-phosphatase:\n")
             taxonomic_group_name = input("Please enter the name of taxonomic group  eg. birds(Aves):\n")
             final_search_term = protein_family_name + " in " + taxonomic_group_name
-            print("\nThe term you enter is:", final_search_term, "\n")
+            print("\nThe term you enter is:", final_search_term,
+                  "\nWe are searching the relevant protein sequence data...\n")
     if tf == 'no':
-            print("\nWe are going to obtain the relevant protein sequence data and analyse them..."+"\n")
+            print("\nWe are searching the relevant protein sequence data...\n")
 
     return protein_family_name,taxonomic_group_name
 
-protein_family_name,taxonomic_group_name =  obtain_search_term()
-#print(protein_family_name)
-#print(taxonomic_group_name)
 
-### obtain the relevant protein sequence data, save to a file
-es_com = "esearch -db protein -query \""+taxonomic_group_name+" [organism] AND "+protein_family_name+" [protein]\"|efetch -format fasta > protein_seq.fa"
+### Assign user's input to variable
+protein_family_name, taxonomic_group_name =  obtain_search_term()
 
-#es_com = "esearch -db protein -query 'aves[organism] AND glucose-6-phosphatase[protein]' |efetch -format fasta > protein_seq.fa"
-#######subprocess.call(es_com,shell=True)
-
-#def _esearch(search_term,email,database):
-    # Retrieve the data set specified by the user in the database
-    # Return information that meets the criteria
-#    Entrez.email = email      ### tell NCBI who you are
-#    handle = Entrez.esearch(db = database, term = search_term)
-#    search_results = Entrez.read(handle)
-#    handle.close()
-#    return search_results
+### Display the result of esearch on the screen
+es_com="esearch -db protein -query \""+taxonomic_group_name+" [organism] AND "+protein_family_name+" [protein]\" > es_result.txt"
+subprocess.call(es_com, shell=True)
+#es_com = "esearch -db protein -query birds [organism] AND glucose-6-phosphatase [protein]\" > es_result.txt"
+#subprocess.call(es_com, shell=True)
 
 
-#search_term = obtain_search_term()   ### glucose-6-phosphatase in birds!!!!!!!!!!!!!!!!!!!!!!
-#print(search_term)
-#search_term = "glucose-6-phosphatase in birds"
-#email = "964145391@qq.com"     ### ask user for email!!!!!!!!!!!!!!!!!!!
-#database = "protein"
-#search_results = _esearch(search_term, email, database)
-
-#def _efetch(search_results,database):
-#    acc_list = search_results["IdList"]
-#    file_name = "protein_seq.fasta"
-#    with open (file_name,'w') as seq:
-#        for _id_ in acc_list:
-#            #print(_id_)
-#            handle = Entrez.efetch(db=database,id=_id_,rettype="fasta",retmode="text")
-#            #print(handle.read())
-#            seq.write(handle.read())  
-#_efetch(search_results,database)
+#subprocess.call("blastp -db selfdb -query pullseq_pro_seq.fa -outfmt 7 > blastoutput.out",shell=True)
 
 
-### alignment
-#os.system("clustalo -i protein_seq.fa --maxnumseq 250 -o ali.fa")
-#subprocess.call("clustalo -i protein_seq.fa --outfmt 1 -o ali.fa", shell=True)
+def total_seq_number():
+    '''
+    Tell the user the total number of sequences
+    Give the user the option to continue or not continue with the current dataset
+
+    '''
+    with open("es_result.txt","r") as es:
+        for line in es.readlines():
+            line = line.strip()     ### Removing leading and trailing whitespace and tabs from a string
+            line = line.strip("<")    ### Removing leading and trailing '<'
+            if re.match(r'C',line):   ### Find the line of <Count> (now beginning with C)
+                total_seq_number = re.findall(r'\d+',line)    ### Grab the number of sequences in <Count>line 
+                for num in total_seq_number: 
+                    ### Tell total sequence number to the user
+                    print("There are "+num+" sequences in the dataset.") 
+                    num = int(num)    ### Convert to int, compare sizes
+                    ### 这里可以返回count的数值
+    return num
+
+total_seq_number = total_seq_number()
+#应该在主程序里写，决定要不要继续，继续就下载，不继续就回到input
+if total_seq_number > 100:  ### 这里可以改
+    print("We recommend that the total number of sequences is less than 1000.\nThe current number is more than 1000.\n")
+    yn=input("Do you want to continue? (please enter 'yes' or 'no'):\n")
+    if yn == 'yes':
+        print("Continuing...Please wait...Don't touch any buttons...")
+        print("We are downloading protein sequences...\n")
+    if yn == 'no':
+        print("Process ended. Please restart the program")
+        sys.exit() 
+
+
+### back to user input                  
+#protein_family_name, taxonomic_group_name =  obtain_search_term()                
+
+
+
+### Obtain the relevant protein sequence data, save to a file
+es_ef_com = "esearch -db protein -query \""+taxonomic_group_name+" [organism] AND "+protein_family_name+" [protein]\"|efetch -format fasta > protein_seq.fa"
+subprocess.call(es_ef_com, shell=True)
+
+### Alignment
 subprocess.call("clustalo -i protein_seq.fa -o ali.fa --force", shell=True)
 
 
 def find_similar_250_seq():
     '''
-    fine the 250 most similar protein sequences
+    When total_seq_num > 250.
+    We need to fine the 250 most similar protein sequences.
 
     '''
-    count_ = 0
-    count_list = [] 
-    nameline = []
-    name_list = []
-
+    count_ = 0  ### Record the number of minus signs
+    count_list = []  ### Store the number of minus signs per sequence
+    nameline = []    ### Store the splited first line of FASTA format 
+    name_list = []   ### Store the name of esch sequence
+    
+    ### Get the sequence name and number of minus signs, stored in two lists, respectively
     with open("ali.fa","r") as alignment:
-        for line in alignment.readlines():    ### Read each line of the file
-            line = line.strip()    ### Remove spaces and tab at the beginning and end of each line
-            if re.match(r'^\>(.*)',line):
+        for line in alignment.readlines():   ### Read each line of the file
+            line = line.strip()    ### Remove spaces and tabs at the beginning and end of each line
+            if re.match(r'^\>(.*)',line):  ### Find the heading line
                 line = line.strip(">")     ### Remove '>' at the begining 
-                nameline = line.split()
+                nameline = line.split()    ### Split the line into a list
                 name_list.append(nameline[0])    ### Extract the sequence name
                 count_list.append(count_)        ### Obtain the number of '-'
                 count_ = 0     ### The counter returns to zero and is ready to count the next '-' number
@@ -97,8 +113,10 @@ def find_similar_250_seq():
     count_list.append(count_)   ### The '-' number of the last sequence was not added to the list in the loop, so it is added here
     del count_list[0]     ### The first element in count_list[] is invalid, so delete it
     #print(count_list,len(count_list),name_list,len(name_list))
-    print(name_list)    
-    ### dictionary {seq_name : number of minus signs}
+    
+
+    ### Merge the data from two lists into a dictionary   
+    ### dictionary count_dict = {seq_name : number of minus signs}
     count_dict = {}
     for i in range(len(name_list)):
         key = name_list[i]
@@ -107,34 +125,50 @@ def find_similar_250_seq():
 
     ### Dictionary sorted by value from small to large
     count_dict_sorted = sorted(count_dict.items(), key=lambda  kv:(kv[1],kv[0]))
-    #print(count_dict_sorted)
+    ### Find the most similar one's name
+    with open ("homo.txt","w") as f:   ### will get "pullseq_1.fa"
+        f.write(count_dict_sorted[0][0])
     
+    ### Find the 250 most similar protein sequences' name
+    for x in range(100):### 这里可以改数字
+        with open ("homo250.txt","a") as fn:   ### will get "pullseq_250.fa"
+            fn.write(count_dict_sorted[x][0]+"\n")
+    #print(count_dict_sorted[0][0])  得到名字大写的。。。.fasta
+        
     ### Get index numbers of most similar seq
-    index_list = [i for i,x in enumerate(count_list) if x==min(count_list)]
+    #index_list = [i for i,x in enumerate(count_list) if x==min(count_list)]
     ### Save the name of the most conservative seq to a txt file 
     #for n in index_list:
     #    with open("homo.txt","a") as f:
     #        f.write(name_list[n]+"\n")
                 
-    with open("homo.txt","w+") as f:
-        f.write(name_list[0])
+    #with open("homo.txt","w+") as f:
+    #    f.write(name_list[0])
 
+ 
+    ### 获得250个序列的名称，从原始文件提取出来250个序列，然后进行对齐，然后画图
+    subprocess.call("/localdisk/data/BPSM/Assignment2/pullseq -i ali.fa -n homo250.txt > pullseq_250.fa", shell=True)
+    subprocess.call("/localdisk/data/BPSM/Assignment2/pullseq -i protein_seq.fa -n homo.txt > pullseq_1.fa", shell=True)
 
-    return count_dict
 find_similar_250_seq()
+### Extrace similar sequences
+#subprocess.call("/localdisk/data/BPSM/Assignment2/pullseq -i protein_seq.fa -n homo.txt > pullseq_1.fa", shell=True)
 
-### Extrace sequences in homo.txt 
-subprocess.call("/localdisk/data/BPSM/Assignment2/pullseq -i protein_seq.fa -n homo.txt > pullseq_pro_seq.fa", shell=True)
 
 ### make a database
 subprocess.call("makeblastdb -in protein_seq.fa -dbtype prot -out selfdb", shell=True)
 
+
 ### Run blastp of our pullseq_pro_seq.fa against the selfdb database, using parameters, saved to the file blastoutput.out
-subprocess.call("blastp -db selfdb -query pullseq_pro_seq.fa -outfmt 7 > blastoutput.out", shell=True)
+subprocess.call("blastp -db selfdb -query pullseq_1.fa -outfmt 7 > blastoutput.out", shell=True)
+
 
 ### Plot conservation of a sequence alignment
+### Alignment again
+#subprocess.call("clustalo -i pullseq_250.fa -o ali.fa --force", shell=True)
 ### get a similarity plot of aligned sequences(plotcon.svg)
-subprocess.call("plotcon -sequence ali.fa -winsize 5 -graph svg", shell=True)
+subprocess.call("plotcon -sequence pullseq_250.fa -winsize 5 -graph svg", shell=True)
+
 ### show the graph
 subprocess.call("eog plotcon.svg", shell=True)
 
@@ -143,10 +177,11 @@ subprocess.call("eog plotcon.svg", shell=True)
 subprocess.call("seqretsplit -sequence protein_seq.fa -sformat fasta -osformat fasta",shell=True)
 
 
+
 ### Scan a protein sequence with motifs from the PROSITE database
 def motifs():
     '''
-    1. obtain name of each sequence
+    1. obtain name of each protein sequence
     2. run patmatmotifs : Scan a protein sequence with motifs from the PROSITE database
     '''
 
